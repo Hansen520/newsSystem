57ce3b3aa2890dc7af189af3588bc50e4c0adb9d
Hansen <646380243@qq.com>
2022-05-11 23:23:49 +0800

    左侧菜单

--- src/components/sandbox/SideMenu.js
+++ src/components/sandbox/SideMenu.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Layout, Menu } from 'antd'
 import './index.css'
 import {
@@ -7,64 +7,101 @@ import {
     UploadOutlined
 } from '@ant-design/icons'
 import { withRouter } from 'react-router-dom'
+import axios from 'axios'
 const { Sider } = Layout
 
 //模拟数组结构
-const  menuList = [
-    {
-      key:"/home",
-      label:"首页",
-      icon:<UserOutlined />
-    },
-    {
-      key:"/user-manage",
-      label:"用户管理",
-      icon:<VideoCameraOutlined />,
-      children:[
-        {
-          key:"/user-manage/list",
-          label:"用户列表",
-          icon:<VideoCameraOutlined />
-        }
-      ]
-    },
-    {
-      key:"/right-manage",
-      label:"权限管理",
-      icon:<UserOutlined />,
-      children:[
-        {
-          key:"/right-manage/role/list",
-          label:"角色列表",
-          icon:<UploadOutlined />
-        },
-        {
-          key:"/right-manage/right/list",
-          label:"权限列表",
-          icon:<UploadOutlined />
-        }
-      ]
-    }
-  ]
-
+// const  menuList = [
+//     {
+//       key:"/home",
+//       label:"首页",
+//       icon:<UserOutlined />
+//     },
+//     {
+//       key:"/user-manage",
+//       label:"用户管理",
+//       icon:<VideoCameraOutlined />,
+//       children:[
+//         {
+//           key:"/user-manage/list",
+//           label:"用户列表",
+//           icon:<VideoCameraOutlined />
+//         }
+//       ]
+//     },
+//     {
+//       key:"/right-manage",
+//       label:"权限管理",
+//       icon:<UserOutlined />,
+//       children:[
+//         {
+//           key:"/right-manage/role/list",
+//           label:"角色列表",
+//           icon:<UploadOutlined />
+//         },
+//         {
+//           key:"/right-manage/right/list",
+//           label:"权限列表",
+//           icon:<UploadOutlined />
+//         }
+//       ]
+//     }
+//   ]
+const iconList = {
+  "/home":<UserOutlined />,
+  "/user-manage":<VideoCameraOutlined />,
+  "/user-manage/list":<UploadOutlined />,
+  "/right-manage":<UserOutlined />,
+  "/right-manage/role/list":<VideoCameraOutlined />,
+  "/right-manage/right/list":<UploadOutlined />
+  //.......
+}
 
 function SideMenu(props) {
-    return (
-        <Sider trigger={null} collapsible >
+
+  const [menu, setMenu] = useState([])
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights?_embed=children').then((res) => {
+      // console.log(res.data)
+      setMenu(res.data)
+    })
+  }, [])
+  // const selectKeys = [props.location.pathname]
+  
+  const handleMenu = (menu) => {
+    menu.forEach((item) => {
+      item.label = item.title
+      item.icon = iconList[item.key]
+      delete item.rightId
+      if(item.children && item.children.length > 0) {
+        handleMenu(item.children)
+      } else {
+        item.children = ''
+      }
+    })
+  }
+  handleMenu(menu)
+  
+
+  return (
+      <Sider trigger={null} collapsible collapsed={false} >
+      <div style={{display:"flex",height:"100%","flexDirection":"column"}}>
         <div className="logo">全球新闻发布管理系统</div>
-        <Menu
-          theme="dark"
-          mode="inline"
-          defaultSelectedKeys={['3']}
-          
-          items={
-            menuList
-          }
-          onClick={(e) => props.history.push(e.key)}
-        >
-        </Menu>
-      </Sider>
-    )
+        <div style={{flex:1,"overflow":"auto"}}>
+          <Menu
+            theme="dark"
+            mode="inline"
+            defaultSelectedKeys={['1']}
+            items={
+              menu
+            }
+            onClick={(e) => props.history.push(e.key)}
+          >
+          </Menu>
+        </div>
+      </div>
+    </Sider>
+  )
 }
 
 export default withRouter(SideMenu)
